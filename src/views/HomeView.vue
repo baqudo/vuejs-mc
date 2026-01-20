ea216a365faddd74815b25bca1d6283f9c878b2b
Oleksandr Balyk <obalyk93@gmail.com>
2026-01-20 17:16:46 +0200

    Add routing and views for home, projects, and single project pages

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Welcome to the Home View!</h1>
+    <p>This is the main landing page of the application.</p>
+    <a href="/projects">go to projects</a>
+    <br />
+    <router-link to="/projects">to projects</router-link>
+  </div>
+</template>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Welcome to the Projects View!</h1>
+    <a href="/">go to home</a>
+    <br />
+    <router-link to="/">to home</router-link>
+  </div>
+</template>
