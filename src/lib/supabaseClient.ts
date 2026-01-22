f52b66fd0f40011074701927e1db2f5ebb30802f
Oleksandr Balyk <obalyk93@gmail.com>
2026-01-23 01:52:58 +0200

    feat: add Supabase types and enhance projects view with loading state and error handling

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
+import type { Database } from '@/types/database'
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY)
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,14 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { supabase } from '@/lib/supabaseClient'
+import type { Tables } from '@/types/database'
+
+const projects = ref<Tables<'projects'>[] | null>(null);
+
+; (async () => {
+  const { data, error } = await supabase.from('projects').select()
+  if (error) {
+    console.error('Error fetching projects:', error)
+  }
+  projects.value = data || []
+  console.log('Projects data:', data)
+})()
+</script>
 
 <template>
   <div>
     <h1>Welcome to the Projects View!</h1>
 
-    <br />
-    <router-link to="/">To Home</router-link>
-    <br />
-    <router-link :to="{ name: '/projects/[id]', params: { id: '1' } }">Project 1</router-link>
-    <br />
-    <router-link :to="{ name: '/projects/[id]', params: { id: '2' } }">Project 2</router-link>
+    <div v-if="projects">
+      <h2>Projects List:</h2>
+      <ul>
+        <li v-for="project in projects" :key="project.id">
+          <router-link :to="{ name: '/projects/[id]', params: { id: project.id } }">{{ project.name }}</router-link>
+          <p>Status: {{ project.status }}</p>
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <p>Loading projects...</p>
+    </div>
   </div>
 </template>
