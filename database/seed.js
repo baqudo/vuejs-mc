95c459f3be2a9d79e3fd17d7f0d2e4450c396c44
Oleksandr Balyk <obalyk93@gmail.com>
2026-01-23 02:07:20 +0200

    feat: implement tasks seeding and update database schema for tasks and projects

--- database/seed.js
+++ database/seed.js
@@ -2,10 +2,7 @@
 import { fakerEN_US as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = process.env.VITE_SUPABASE_URL
-const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY)
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SUPABASE_API_KEY)
 
 const logErrorAndExit = (tableName, error) => {
   console.error(
@@ -27,7 +24,7 @@ const seedProjects = async (numEntries) => {
 
     projects.push({
       name: name,
-      slug: faker.helpers.slugify(name).toLowerCase(),
+      slug: name.toLocaleLowerCase().replace(/ /g, '-'),
       status: faker.helpers.arrayElement(['in-progress', 'completed']),
       collaborators: faker.helpers.arrayElements([1, 2, 3]),
     })
@@ -42,8 +39,33 @@ const seedProjects = async (numEntries) => {
   return data
 }
 
+const seedTasks = async (numEntries, projectsIds) => {
+  logStep('Seeding tasks...')
+  const tasks = []
+
+  for (let i = 0; i < numEntries; i++) {
+    tasks.push({
+      name: faker.lorem.words(3),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      description: faker.lorem.paragraph(),
+      due_date: faker.date.future(),
+      project_id: faker.helpers.arrayElement(projectsIds),
+      collaborators: faker.helpers.arrayElements([1, 2, 3]),
+    })
+  }
+
+  const { data, error } = await supabase.from('tasks').insert(tasks).select('id')
+
+  if (error) return logErrorAndExit('Tasks', error)
+
+  logStep('Tasks seeded successfully.')
+
+  return data
+}
+
 const seedDatabase = async (numEntriesPerTable) => {
-  await seedProjects(numEntriesPerTable)
+  const projectsIds = (await seedProjects(numEntriesPerTable)).map((project) => project.id)
+  await seedTasks(numEntriesPerTable, projectsIds)
 }
 
 const numEntriesPerTable = 10
