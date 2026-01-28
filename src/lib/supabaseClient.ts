391dca45ab657f63626e65f0ece2af84672004b2
Oleksandr Balyk <obalyk93@gmail.com>
2026-01-28 17:02:43 +0200

    feat: add environment variables for Supabase configuration and implement primary test user creation in seed script

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 import type { Database } from '@/types/database'
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_KEY as string
 // Create a single supabase client for interacting with your database
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY)
--- supabase/migrations/20260122235450_projects_schema.sql
+++ supabase/migrations/20260122235450_projects_schema.sql
@@ -3,7 +3,7 @@ drop table if exists projects;
 drop type if exists current_status;
 create type current_status as enum ('in-progress', 'completed');
 
-create table
+create table 
   projects (
     id bigint primary key generated always as identity not null,
     created_at timestamptz default now() not null,
